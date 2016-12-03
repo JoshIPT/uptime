d4a9a802f3e8b8551fd0c046e85e5b82ab5c6c7a
Josh <josh@tptech.com.au>
2016-12-04 03:29:15 +1000

    Initial commit

---
+++ uptime/uptime/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Reflection;
+using System.Management;
+using System.Diagnostics;
+
+namespace uptime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeSpan upspan;
+            /*using (var uptime = new PerformanceCounter("System", "System Up Time"))
+            {
+                uptime.NextValue(); 
+                upspan = TimeSpan.FromSeconds(uptime.NextValue());
+            }*/
+
+            var ticks = Stopwatch.GetTimestamp();
+            var uptime = ((double)ticks) / Stopwatch.Frequency;
+            upspan = TimeSpan.FromSeconds(uptime);
+
+            string upstr = upspan.Days.ToString() + " days, ";
+            upstr += padInt(upspan.Hours) + ":";
+            upstr += padInt(upspan.Minutes) + ":";
+            upstr += padInt(upspan.Seconds);
+
+            Console.WriteLine("Uptime: " + upstr);
+
+            /*if ((!isParentCmd()) && (args.Length < 2))
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
+            }*/
+        }
+
+        public static string padInt(int num)
+        {
+            if (num < 10)
+            {
+                return "0" + num.ToString();
+            }
+            else
+            {
+                return num.ToString();
+            }
+        }
+
+        public static bool isParentCmd()
+        {
+            var myId = Process.GetCurrentProcess().Id;
+            var query = string.Format("SELECT ParentProcessId FROM Win32_Process WHERE ProcessId = {0}", myId);
+            var search = new ManagementObjectSearcher("root\\CIMV2", query);
+            var results = search.Get().GetEnumerator();
+            results.MoveNext();
+            var queryObj = results.Current;
+            var parentId = (uint)queryObj["ParentProcessId"];
+            var parent = Process.GetProcessById((int)parentId);
+            if (parent.ProcessName.ToLower() == "cmd") { return true; }
+            else { return false; }
+        }
+    }
+}
---
+++ uptime/uptime/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("uptime")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Turning Point Technology")]
+[assembly: AssemblyProduct("uptime")]
+[assembly: AssemblyCopyright("Copyright © 2016 Turning Point Technology")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("785353ca-b4e5-4712-8a61-14c8e3f73b53")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
